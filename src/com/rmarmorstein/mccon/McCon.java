66c017c74f192211492e2ae8a076e0f1f668bbca
River <rivermarmorstein@gmail.com>
2013-12-29 00:46:16 -0500

    updates

--- src/com/rmarmorstein/mccon/McCon.java
+++ src/com/rmarmorstein/mccon/McCon.java
@@ -1,31 +1,33 @@
+/**
+ * McCon is Licensed under 
+ * 
+ * The Wonderful contributors
+ * @author River Marmorstein
+ * @author Rmarmorstein
+ */
+
+
+
 package com.rmarmorstein.mccon;
 
-import java.sql.SQLException;
 import java.util.logging.Logger;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class McCon extends JavaPlugin {
 	
-	public Logger log;
-	public static boolean debugmode = false;
-	private Database db;
+	private Logger log;
 	
 	@Override
 	public void onEnable() {
 		log = this.getLogger();
-		if(this.getConfig().getBoolean("debug")) {
-			debugmode = true;
-		}
-		Database db = new Database(this);
-		this.db = db;
-		log.info("McCon: v. " + this.getDescription().getVersion() + " has been loaded");
+		log.info("Starting Initialization of McCon v. " + this.getDescription().getVersion());
 	}
 	
 	@Override
 	public void onDisable() {
 		
 	}
+	
 
 }
