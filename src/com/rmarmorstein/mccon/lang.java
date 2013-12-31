7f3fb5ec22d18dd2078b89adfb5155471e799276
River <rivermarmorstein@gmail.com>
2013-12-31 04:16:35 -0500

    add new lang file to the repo

---
+++ src/com/rmarmorstein/mccon/lang.java
@@ -0,0 +1,14 @@
+package com.rmarmorstein.mccon;
+
+import org.bukkit.ChatColor;
+
+public final class lang {
+	
+	private McCon plugin;
+	
+	public lang(McCon instance) { 
+		this.plugin = instance;
+	}
+	
+	
+}
